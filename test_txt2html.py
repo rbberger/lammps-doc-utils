4e463f973d2ed948a1eead740b6d6f928fd088d7
Richard Berger <richard.berger@jku.at>
2015-06-24 19:56:34 +0200

    Refactored test to use setUp

--- test_txt2html.py
+++ test_txt2html.py
@@ -2,12 +2,11 @@ import unittest
 from txt2html import Txt2Html
 
 class TestBasicFormatting(unittest.TestCase):
-    def test_create_instance(self):
-        t = Txt2Html()
+    def setUp(self):
+        self.txt2html = Txt2Html()
 
     def test_empty_string(self):
-        t = Txt2Html()
-        self.assertEquals(t.convert(""), "<HTML>\n</HTML>\n")
+        self.assertEquals(self.txt2html.convert(""), "<HTML>\n</HTML>\n")
 
 if __name__ == '__main__':
     unittest.main()
