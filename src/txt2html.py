10dbbd1a47d662f22327685203c607de1586bd8f
Richard Berger <richard.berger@jku.at>
2015-07-16 08:22:55 +0200

    Ensure Python3 is used by scripts

--- src/txt2html.py
+++ src/txt2html.py
@@ -1,4 +1,4 @@
-#! /bin/env python
+#! /bin/env python3
 # Python rewrite of txt2html by Richard Berger (2015)
 # The original txt2html was written in C by Steve Plimpton (http://www.cs.sandia.gov/cgi-bin/sjplimp/)
 import os
