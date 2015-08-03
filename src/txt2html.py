7f6bf5189cf133463cd580a75d4477ad21844fff
Richard Berger <richard.berger@jku.at>
2015-08-04 01:39:36 +0200

    Added file headers

--- src/txt2html.py
+++ src/txt2html.py
@@ -1,6 +1,25 @@
 #! /bin/env python3
-# Python rewrite of txt2html by Richard Berger (2015)
-# The original txt2html was written in C by Steve Plimpton (http://www.cs.sandia.gov/cgi-bin/sjplimp/)
+# LAMMPS Documentation Utilities
+#
+# Python rewrite of txt2html
+# The original txt2html was written in C by Steve Plimpton
+# (http://www.cs.sandia.gov/cgi-bin/sjplimp/)
+#
+# Copyright (C) 2015 Richard Berger
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 import os
 import re
 import sys
