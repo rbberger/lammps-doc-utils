aeabb4ab3f24783efd48565f2b283ff7bdbfd53c
Richard Berger <richard.berger@jku.at>
2015-08-13 23:04:41 +0200

    Remove disabled tests

--- src/test_lammps_filters.py
+++ src/test_lammps_filters.py
@@ -22,10 +22,6 @@ class TestStructuralFilters(unittest.TestCase):
     def setUp(self):
         self.txt2rst = txt2rst.Txt2Rst()
 
-    def disable_test_filter_local_toc(self):
-        s = self.txt2rst.convert("1.0 Title<BR>\n")
-        self.assertEqual("", s)
-
     def test_detect_and_replace_warnings(self):
         s = self.txt2rst.convert("IMPORTANT NOTE: Content\n")
         self.assertEqual(".. warning::\n\n"
